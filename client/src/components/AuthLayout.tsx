61849b0ff1cdd981316235d5207bea6cf563a824
logy-dev-one <carlospegorarolopes@gmail.com>
2024-09-28 19:34:43 -0300

    + add: types in components

--- client/src/components/AuthLayout.tsx
+++ client/src/components/AuthLayout.tsx
@@ -1,6 +1,11 @@
 import Logo from "./Logo";
+import { ReactNode } from 'react';
 
-export default function AuthLayout({children}) {
+interface AuthLayoutProps {
+    children: ReactNode;
+  }
+
+export default function AuthLayout({children}: AuthLayoutProps) {
     return (
         <div className="flex items-center justify-around w-full min-h-screen p-10 bg-gradient-to-bl from-slate-100 to-slate-200">
             <div>
--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,4 +1,10 @@
-export default function ButtonPrimary({children}) {
+import { ReactNode } from 'react';
+
+interface ButtonPrimaryProps {
+  children: ReactNode;
+}
+
+export default function ButtonPrimary({children} : ButtonPrimaryProps) {
     return (
         <button className="px-2 py-3 rounded-md bg-sky-500 hover:bg-sky-600 text-slate-100" type="submit">{children}</button>
     )
